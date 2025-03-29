6fa1ac500f3e332b90002e992bb84cda9ceaf305
rahul <seorahul007@gmail.com>
2025-03-29 18:47:50 +0530

    Day 6

--- Seleniumprogramming/src/demopractice/firstprogram.java
+++ Seleniumprogramming/src/demopractice/firstprogram.java
@@ -2,6 +2,9 @@ package demopractice;
 
 public class firstprogram {
 
+	//Declare Array
+	//add value into array
+	
 	public static void main(String[] args) {
 		System.out.println("Hii..shifting to branch");
 
